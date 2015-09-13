3f76d889651be520018b6cb72eee61cca79650cf
badromance007 <nguyenpeter.edu@gmail.com>
2015-09-13 21:27:11 +0700

    creating login form

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -3,7 +3,7 @@ class Dashboard extends Admin_Controller {
 
 	public function __construct(){
 		parent::__construct();
-		$this->load->helper('url_helper');
+		//$this->load->helper('url_helper');
 	}
 
 	public function index() {
--- application/views/admin/_layout_modal.php
+++ application/views/admin/_layout_modal.php
@@ -5,13 +5,9 @@
   <div class="modal show">
     <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h3 class="modal-title">Modal title</h3>
-        </div>
-        <div class="modal-body">
-          <p>One fine body&hellip;</p>
-        </div>
+
+<?php $this->load->view($subview); // Subview is set in controller ?>
+
         <div class="modal-footer">
           &copy; <?php echo $meta_title; ?>
         </div>
